7ba71b73c87fd4158e6ef29119436a11b42e5f04
David Storey <david@lostincode.co.uk>
2015-02-28 10:21:14 +0000

    Reorganised channel to be easier to scan

--- back_end/config/channels.js
+++ back_end/config/channels.js
@@ -1,445 +1,78 @@
 module.exports = {
-	2082: {
-		freview_id: 101,
-		sky_id:     2082,
-		title:      'BBC One ScotHD',
-		family:     'bbc',
-		region:     'scotland'
-	},
-	1626: {
-		freview_id: 104,
-		sky_id:     1626,
-		title:      'Channel 4',
-		family:     'four'
-	}
-	,
-	1830: {
-		freview_id: 105,
-		sky_id:     1830,
-		title:      'Channel 5',
-		family:     'five'
-	},
-	2077: {
-		freview_id: 115,
-		sky_id:     2077,
-		title:      'BBC Three HD',
-		family:     'bbc'
-	},
-	2086: {
-		freview_id: 116,
-		sky_id:     2086,
-		title:      'BBC Four HD',
-		family:     'bbc'
-	},
-	1675: {
-		freview_id: 135,
-		sky_id:     1675,
-		title:      'Channel 4 +1',
-		family:     'four'
-	},
-	3300: {
-		freview_id: 137,
-		sky_id:     3300,
-		title:      'E4+1',
-		family:     'four'
-	},
-	3310: {
-		freview_id: 139,
-		sky_id:     3310,
-		title:      'More4 +1',
-		family:     'four'
-	},
-	3150: {
-		freview_id: 140,
-		sky_id:     3150,
-		title:      '4seven',
-		family:     'four'
-	},
-	2075: {
-		freview_id: 142,
-		sky_id:     2075,
-		title:      'BBC Two HD',
-		family:     'bbc'
-	},
-	4610: {
-		freview_id: 148,
-		sky_id:     4610,
-		title:      'CBS Action',
-		family:     'other'
-	},
-	3617: {
-		freview_id: 149,
-		sky_id:     3617,
-		title:      'CBS Drama',
-		family:     'other'
-	},
-	1832: {
-		freview_id: 152,
-		sky_id:     1832,
-		title:      'Pick',
-		family:     'other'
-	},
-	1836: {
-		freview_id: 153,
-		sky_id:     1836,
-		title:      'Pick+1',
-		family:     'other'
-	},
-	3022: {
-		freview_id: 174,
-		sky_id:     3022,
-		title:      '5 USA',
-		family:     'five'
-	},
-	3028: {
-		freview_id: 175,
-		sky_id:     3028,
-		title:      'Channel 5+24',
-		family:     'five'
-	},
-	3023: {
-		freview_id: 176,
-		sky_id:     3023,
-		title:      '5*',
-		family:     'five'
-	},
-	1839: {
-		freview_id: 177,
-		sky_id:     1839,
-		title:      'Channel 5+1',
-		family:     'five'
-	},
-	6241: {
-		freview_id: 179,
-		sky_id:     6241,
-		title:      'ITV2+1',
-		family:     'itv'
-	},
-	6261: {
-		freview_id: 180,
-		sky_id:     6261,
-		title:      'ITV3+1',
-		family:     'itv'
-	},
-	5338: {
-		freview_id: 183,
-		sky_id:     5338,
-		title:      'True Drama',
-		family:     'other'
-	},
-	4266: {
-		freview_id: 184,
-		sky_id:     4266,
-		title:      'True Ent',
-		family:     'other'
-	},
-	3541: {
-		freview_id: 185,
-		sky_id:     3541,
-		title:      'more>movies',
-		family:     'other'
-	},
-	3220: {
-		freview_id: 186,
-		sky_id:     3220,
-		title:      'm>movies+1',
-		family:     'other'
-	},
-	1628: {
-		freview_id: 202,
-		sky_id:     1628,
-		title:      'E4',
-		family:     'four'
-	},
-	6274: {
-		freview_id: 206,
-		sky_id:     6274,
-		title:      'ITV4+1',
-		family:     'itv'
-	},
-	6240: {
-		freview_id: 207,
-		sky_id:     6240,
-		title:      'ITV2',
-		family:     'itv'
-	},
-	6260: {
-		freview_id: 208,
-		sky_id:     6260,
-		title:      'ITV3',
-		family:     'itv'
-	},
-	2061: {
-		freview_id: 210,
-		sky_id:     2061,
-		title:      'BBC Three',
-		family:     'bbc'
-	},
-	2018: {
-		freview_id: 211,
-		sky_id:     2018,
-		title:      'BBC Four',
-		family:     'bbc'
-	},
-	3027: {
-		freview_id: 219,
-		sky_id:     3027,
-		title:      '5 USA +1',
-		family:     'five'
-	},
-	6272: {
-		freview_id: 228,
-		sky_id:     6272,
-		title:      'ITV4',
-		family:     'itv'
-	},
-	4075: {
-		freview_id: 230,
-		sky_id:     4075,
-		title:      'Channel 4 HD',
-		family:     'four'
-	},
-	3340: {
-		freview_id: 236,
-		sky_id:     3340,
-		title:      'More4',
-		family:     'four'
-	},
-	5201: {
-		freview_id: 292,
-		sky_id:     5201,
-		title:      'FilmOn.TV',
-		family:     'other'
-	},
-	5297: {
-		freview_id: 293,
-		sky_id:     5297,
-		title:      'Showcase 2 +1',
-		family:     'other'
-	},
-	1629: {
-		freview_id: 316,
-		sky_id:     1629,
-		title:      'Film4 +1',
-		family:     'four'
-	},
-	3605: {
-		freview_id: 319,
-		sky_id:     3605,
-		title:      'horror channel',
-		family:     'other'
-	},
-	4502: {
-		freview_id: 320,
-		sky_id:     4502,
-		title:      'horror ch+1',
-		family:     'other'
-	},
-	3643: {
-		freview_id: 321,
-		sky_id:     3643,
-		title:      'True Movies 1',
-		family:     'other'
-	},
-	3751: {
-		freview_id: 322,
-		sky_id:     3751,
-		title:      'True Movies 2',
-		family:     'other'
-	},
-	3709: {
-		freview_id: 323,
-		sky_id:     3709,
-		title:      'Sony Movies',
-		family:     'other'
-	},
-	3771: {
-		freview_id: 324,
-		sky_id:     3771,
-		title:      'Sony Movies+1',
-		family:     'other'
-	},
-	3708: {
-		freview_id: 325,
-		sky_id:     3708,
-		title:      'movies4men',
-		family:     'other'
-	},
-	3721: {
-		freview_id: 326,
-		sky_id:     3721,
-		title:      'mov4men+1',
-		family:     'other'
-	},
-	1627: {
-		freview_id: 342,
-		sky_id:     1627,
-		title:      'Film4',
-		family:     'four'
-	},
-	2004: {
-		freview_id: 951,
-		sky_id:     2004,
-		title:      'BBC One Scot',
-		family:     'bbc',
-		region:     'scotland'
-	},
-	2003: {
-		freview_id: 952,
-		sky_id:     2003,
-		title:      'BBC One Wales',
-		family:     'bbc',
-		region:     'wales'
-	},
-	2005: {
-		freview_id: 953,
-		sky_id:     2005,
-		title:      'BBC One NI',
-		family:     'bbc',
-		region:     'nireland'
-	},
-	2002: {
-		freview_id: 954,
-		sky_id:     2002,
-		title:      'BBC One Lon',
-		family:     'bbc',
-		region:     'england'
-	},
-	2155: {
-		freview_id: 955,
-		sky_id:     2155,
-		title:      'BBC One NE&C',
-		family:     'bbc'
-	},
-	2104: {
-		freview_id: 956,
-		sky_id:     2104,
-		title:      'BBC One Yorks',
-		family:     'bbc'
-	},
-	2103: {
-		freview_id: 957,
-		sky_id:     2103,
-		title:      'BBC One Yk&Li',
-		family:     'bbc'
-	},
-	2102: {
-		freview_id: 958,
-		sky_id:     2102,
-		title:      'BBC One N West',
-		family:     'bbc'
-	},
-	2101: {
-		freview_id: 959,
-		sky_id:     2101,
-		title:      'BBC One W Mid',
-		family:     'bbc'
-	},
-	2105: {
-		freview_id: 960,
-		sky_id:     2105,
-		title:      'BBC One E Mid',
-		family:     'bbc'
-	},
-	2106: {
-		freview_id: 961,
-		sky_id:     2106,
-		title:      'BBC One East E',
-		family:     'bbc'
-	},
-	2073: {
-		freview_id: 962,
-		sky_id:     2073,
-		title:      'BBC One East W',
-		family:     'bbc'
-	},
-	2152: {
-		freview_id: 963,
-		sky_id:     2152,
-		title:      'BBC One S East',
-		family:     'bbc'
-	},
-	2153: {
-		freview_id: 964,
-		sky_id:     2153,
-		title:      'BBC One South',
-		family:     'bbc'
-	},
-	2156: {
-		freview_id: 965,
-		sky_id:     2156,
-		title:      'BBC One Oxford',
-		family:     'bbc'
-	},
-	2151: {
-		freview_id: 966,
-		sky_id:     2151,
-		title:      'BBC One West',
-		family:     'bbc'
-	},
-	2154: {
-		freview_id: 967,
-		sky_id:     2154,
-		title:      'BBC One S West',
-		family:     'bbc'
-	},
-	2074: {
-		freview_id: 968,
-		sky_id:     2074,
-		title:      'BBC One CI',
-		family:     'bbc'
-	},
-	2006: {
-		freview_id: 969,
-		sky_id:     2006,
-		title:      'BBC Two Eng',
-		family:     'bbc',
-		region:     'england'
-	},
-	2016: {
-		freview_id: 970,
-		sky_id:     2016,
-		title:      'BBC Two Scot',
-		family:     'bbc',
-		region:     'scotland'
-	},
-	2015: {
-		freview_id: 971,
-		sky_id:     2015,
-		title:      'BBC Two Wales',
-		family:     'bbc',
-		region:     'wales'
-	},
-	2017: {
-		freview_id: 972,
-		sky_id:     2017,
-		title:      'BBC Two NI',
-		family:     'bbc',
-		region:     'nireland'
-	},
-	6000: {
-		freview_id: 973,
-		sky_id:     6000,
-		title:      'ITV',
-		family:     'itv'
-	},
-	// 1621: {
-	// 	freview_id: 974,
-	// 	sky_id:     1621,
-	// 	title:      'Channel 4',
-	// 	family:     'four'
-	// },
-	// 1670: {
-	// 	freview_id: 975,
-	// 	sky_id:     1670,
-	// 	title:      'Channel 4 +1',
-	// 	family:     'four'
-	// },
-	2076: {
-		freview_id: 976,
-		sky_id:     2076,
-		title:      'BBC One HD',
-		family:     'bbc',
-		region:     'england'
-	}
-};
+   2004: { freview_id: 951, sky_id: 2004, title: 'BBC One',         family: 'bbc',  include: 'scotland'}, // BBC One Scot
+   2003: { freview_id: 952, sky_id: 2003, title: 'BBC One',         family: 'bbc',  include: 'wales'},    // BBC One Wales
+   2005: { freview_id: 953, sky_id: 2005, title: 'BBC One',         family: 'bbc',  include: 'nireland'}, // BBC One NI
+   2002: { freview_id: 954, sky_id: 2002, title: 'BBC One',         family: 'bbc',  include: 'england'},  // BBC One Lon
+// 2155: { freview_id: 955, sky_id: 2155, title: 'BBC One NE&C',    family: 'bbc'   },
+// 2104: { freview_id: 956, sky_id: 2104, title: 'BBC One Yorks',   family: 'bbc'   },
+// 2103: { freview_id: 957, sky_id: 2103, title: 'BBC One Yk&Li',   family: 'bbc'   },
+// 2102: { freview_id: 958, sky_id: 2102, title: 'BBC One N West',  family: 'bbc'   },
+// 2101: { freview_id: 959, sky_id: 2101, title: 'BBC One W Mid',   family: 'bbc'   },
+// 2105: { freview_id: 960, sky_id: 2105, title: 'BBC One E Mid',   family: 'bbc'   },
+// 2106: { freview_id: 961, sky_id: 2106, title: 'BBC One East E',  family: 'bbc'   },
+// 2073: { freview_id: 962, sky_id: 2073, title: 'BBC One East W',  family: 'bbc'   },
+// 2152: { freview_id: 963, sky_id: 2152, title: 'BBC One S East',  family: 'bbc'   },
+// 2153: { freview_id: 964, sky_id: 2153, title: 'BBC One South',   family: 'bbc'   },
+// 2156: { freview_id: 965, sky_id: 2156, title: 'BBC One Oxford',  family: 'bbc'   },
+// 2151: { freview_id: 966, sky_id: 2151, title: 'BBC One West',    family: 'bbc'   },
+// 2154: { freview_id: 967, sky_id: 2154, title: 'BBC One S West',  family: 'bbc'   },
+// 2074: { freview_id: 968, sky_id: 2074, title: 'BBC One CI',      family: 'bbc'   },
+   2016: { freview_id: 970, sky_id: 2016, title: 'BBC Two',         family: 'bbc',  include: 'scotland'}, // BBC Two Scot
+   2015: { freview_id: 971, sky_id: 2015, title: 'BBC Two',         family: 'bbc',  include: 'wales'},    // BBC Two Wales
+   2017: { freview_id: 972, sky_id: 2017, title: 'BBC Two',         family: 'bbc',  include: 'nireland'}, // BBC Two NI
+   2006: { freview_id: 969, sky_id: 2006, title: 'BBC Two',         family: 'bbc',  include: 'england'},  // BBC Two Eng
+   2061: { freview_id: 210, sky_id: 2061, title: 'BBC Three',       family: 'bbc'   },
+   2018: { freview_id: 211, sky_id: 2018, title: 'BBC Four',        family: 'bbc'   }, 
+   2082: { freview_id: 101, sky_id: 2082, title: 'BBC One HD',      family: 'bbc',  include: 'scotland'}, // BBC One ScotHD
+   2076: { freview_id: 976, sky_id: 2076, title: 'BBC One HD',      family: 'bbc',  exclude: 'scotland'},
+   2075: { freview_id: 142, sky_id: 2075, title: 'BBC Two HD',      family: 'bbc'   },
+   2077: { freview_id: 115, sky_id: 2077, title: 'BBC Three HD',    family: 'bbc'   },
+   2086: { freview_id: 116, sky_id: 2086, title: 'BBC Four HD',     family: 'bbc'   },
+  
+   6000: { freview_id: 973, sky_id: 6000, title: 'ITV',             family: 'itv'   },
+   6240: { freview_id: 207, sky_id: 6240, title: 'ITV2',            family: 'itv'   },
+   6241: { freview_id: 179, sky_id: 6241, title: 'ITV2 +1',         family: 'itv'   },
+   6260: { freview_id: 208, sky_id: 6260, title: 'ITV3',            family: 'itv'   },
+   6261: { freview_id: 180, sky_id: 6261, title: 'ITV3 +1',         family: 'itv'   },
+   6272: { freview_id: 228, sky_id: 6272, title: 'ITV4',            family: 'itv'   },
+   6274: { freview_id: 206, sky_id: 6274, title: 'ITV4 +1',         family: 'itv'   },
+  
+   1626: { freview_id: 104, sky_id: 1626, title: 'Channel 4',       family: 'four'  },
+   1675: { freview_id: 135, sky_id: 1675, title: 'Channel 4 +1',    family: 'four'  },
+   4075: { freview_id: 230, sky_id: 4075, title: 'Channel 4 HD',    family: 'four'  },
+   1628: { freview_id: 202, sky_id: 1628, title: 'E4',              family: 'four'  },
+   3300: { freview_id: 137, sky_id: 3300, title: 'E4 +1',           family: 'four'  },
+   3340: { freview_id: 236, sky_id: 3340, title: 'More4',           family: 'four'  },
+   3310: { freview_id: 139, sky_id: 3310, title: 'More4 +1',        family: 'four'  },
+   3150: { freview_id: 140, sky_id: 3150, title: '4seven',          family: 'four'  },
+   1627: { freview_id: 342, sky_id: 1627, title: 'Film4',           family: 'four'  },
+   1629: { freview_id: 316, sky_id: 1629, title: 'Film4 +1',        family: 'four'  },
+// 1621: { freview_id: 974, sky_id: 1621, title: 'Channel 4',       family: 'four'  }, // Duplicate
+// 1670: { freview_id: 975, sky_id: 1670, title: 'Channel 4 +1',    family: 'four'  }, // Duplicate
+  
+   1830: { freview_id: 105, sky_id: 1830, title: 'Channel 5',       family: 'five'  },
+   1839: { freview_id: 177, sky_id: 1839, title: 'Channel 5 +1',    family: 'five'  },
+   3028: { freview_id: 175, sky_id: 3028, title: 'Channel 5 +24',   family: 'five'  },
+   3022: { freview_id: 174, sky_id: 3022, title: '5 USA',           family: 'five'  },
+   3027: { freview_id: 219, sky_id: 3027, title: '5 USA +1',        family: 'five'  },
+   3023: { freview_id: 176, sky_id: 3023, title: '5*',              family: 'five'  },
+  
+   4610: { freview_id: 148, sky_id: 4610, title: 'CBS Action',      family: 'other' },
+   3617: { freview_id: 149, sky_id: 3617, title: 'CBS Drama',       family: 'other' },
+   1832: { freview_id: 152, sky_id: 1832, title: 'Pick',            family: 'other' },
+   1836: { freview_id: 153, sky_id: 1836, title: 'Pick +1',         family: 'other' },
+   5338: { freview_id: 183, sky_id: 5338, title: 'True Drama',      family: 'other' },
+   4266: { freview_id: 184, sky_id: 4266, title: 'True Ent',        family: 'other' },
+   3541: { freview_id: 185, sky_id: 3541, title: 'more>movies',     family: 'other' },
+   3220: { freview_id: 186, sky_id: 3220, title: 'm>movies +1',     family: 'other' },
+   5201: { freview_id: 292, sky_id: 5201, title: 'FilmOn.TV',       family: 'other' },
+   5297: { freview_id: 293, sky_id: 5297, title: 'Showcase 2 +1',   family: 'other' },
+   3605: { freview_id: 319, sky_id: 3605, title: 'horror channel',  family: 'other' },
+   4502: { freview_id: 320, sky_id: 4502, title: 'horror ch +1',    family: 'other' },
+   3643: { freview_id: 321, sky_id: 3643, title: 'True Movies 1',   family: 'other' },
+   3751: { freview_id: 322, sky_id: 3751, title: 'True Movies 2',   family: 'other' },
+   3709: { freview_id: 323, sky_id: 3709, title: 'Sony Movies',     family: 'other' },
+   3771: { freview_id: 324, sky_id: 3771, title: 'Sony Movies +1',  family: 'other' },
+   3708: { freview_id: 325, sky_id: 3708, title: 'movies4men',      family: 'other' },
+   3721: { freview_id: 326, sky_id: 3721, title: 'mov4men+1',       family: 'other' }
+};  
