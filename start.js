210be9b36317ef8ebee87b9f432de87078a631f6
David Storey <david@lostincode.co.uk>
2015-02-28 09:57:56 +0000

    Added command line arg for forcing data update

--- start.js
+++ start.js
@@ -1,4 +1,6 @@
 // DESCRIPTION: Starts the core app functionality
 
+var forceUpdate = (process.argv[2] === 'update');
+
 var webserver  = require('./back_end/webserver')(__dirname); // Start webserver for the front-end
-var films      = require('./back_end/films')(false);     // Start the scheduled job for getting film data
+var films      = require('./back_end/films')(forceUpdate);     // Start the scheduled job for getting film data
