5b215602f4dd077019243f04ad02c467fc5cc479
David Storey <david@lostincode.co.uk>
2015-08-06 20:23:37 +0100

    Parse error responses from servers

--- back_end/helpers/fetch.js
+++ back_end/helpers/fetch.js
@@ -4,7 +4,7 @@ var log     = require('./log');
 
 var queue = []; // Stack up requests here
 var throttleDelay = 500; // Delay in MS between requests
-var maxRetries    = 3; // Number of times to reattempt a failed call
+var maxRetries    = 5; // Number of times to reattempt a failed call
 
 
 module.exports = function(url, data, callback) {
@@ -51,7 +51,7 @@ function makeRequest(item) {
 			item.callback(body);
 			nextItem();
 		} else {
-			log('ERROR fetching ' + item.url + '\n' + JSON.stringify(item.data) + '\nError: ' + error + '\n' + response + '\n' + 'Attempts left: ' + item.retries);
+			log('ERROR fetching ' + item.url + '\n' + JSON.stringify(item.data) + '\nError: ' + error + '\n' + JSON.parse(response) + '\n' + 'Attempts left: ' + item.retries);
 			item.retries--;
 			queue.push(item);
 			nextItem();
