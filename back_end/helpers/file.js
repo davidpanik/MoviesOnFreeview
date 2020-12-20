c94c570fe3915fe8573c3d67d3f356a9992244e0
David Storey <david@lostincode.co.uk>
2020-12-20 10:30:04 +0000

    Got working with new MongoDB

--- back_end/helpers/file.js
+++ back_end/helpers/file.js
@@ -3,13 +3,17 @@ var fs = require('fs');
 module.exports = function(filename) {
 	var fileControl = {
 		reset: function() {
-			fs.writeFile(filename, '');
+			fs.writeFile(filename, '', function (err, data) {
+				if (err) throw err;
+			});
 
 			return this;
 		},
 
 		append: function(content) {
-			fs.appendFile(filename, content);
+			fs.appendFile(filename, content, function (err, data) {
+				if (err) throw err;
+			});
 
 			return this;
 		},
--- back_end/helpers/mongo.js
+++ back_end/helpers/mongo.js
@@ -1,95 +1,130 @@
-var mongojs = require('mongojs');
-var log     = require('./log');
+// https://cloud.mongodb.com/v2/5fccbfb79d765327bbd54d7a#clusters/detail/moviesonfreeview
+// https://cloud.mongodb.com/v2/5fccbfb79d765327bbd54d7a#metrics/replicaSet/5fccc554aecebf0801244a09/explorer/heroku_7qhfvjll/moviesonfreeview_films/find
 
-var uri = 'mongodb+srv://heroku_7qhfvjll:PASSWORD@moviesonfreeview.ijyos.mongodb.net/heroku_7qhfvjll?retryWrites=true&w=majority';
+var log = require('./log');
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = 'mongodb+srv://heroku_7qhfvjll:h4vpbbqsmmr6539psoasin5mkf@moviesonfreeview.ijyos.mongodb.net/heroku_7qhfvjll?retryWrites=false&w=majority'
 var collectionName = 'moviesonfreeview_films';
-var db = mongojs(uri, [collectionName], {authMechanism: 'ScramSHA1'});
-var collection = db.collection(collectionName);
+var client = new MongoClient(uri);
+var collection;
+
+client.connect(function(err) {
+	if (err) {
+		log(err);
+	} else {
+		collection = client.db('heroku_7qhfvjll').collection(collectionName);
+	}
+});
+
+// client.close();
+
+function waitForCollection(callback){
+    if(typeof collection !== 'undefined'){
+        callback();
+    } else {
+        setTimeout(waitForCollection, 250);
+    }
+}
 
 module.exports = {
 	insert: function(obj, callback) {
-		collection.insert(obj, function (err, result) {
-			if (err) {
-				log(err);
-			} else {
-				log('Inserted object into collection');
-			}
-
-			callback();
-		});
+		waitForCollection(function() {
+			collection.insertOne(obj, function (err, result) {
+				if (err) {
+					log(err);
+				} else {
+					log('Inserted object into collection');
+				}
+	
+				callback();
+			});
+		}.bind(this));
 	},
 
 	find: function(query, callback) {
-		collection.find(query).toArray(function (err, result) {
-			if (err) {
-				log(err);
-			} else if (result.length) {
-				log('Found: ', result);
-				callback(result);
-			} else {
-				log('No document(s) found with defined "find" criteria!');
-				callback({});
-			}
-		});
+		waitForCollection(function() {
+			collection.find(query).toArray(function (err, result) {
+				if (err) {
+					log(err);
+				} else if (result.length) {
+					log('Found: ', result);
+					callback(result);
+				} else {
+					log('No document(s) found with defined "find" criteria!');
+					callback({});
+				}
+			});
+		}.bind(this));
 	},
 
 	count: function(query, callback) {
-		collection.find(query).toArray(function (err, result) {
-			if (err) {
-				log(err);
-			} else if (result.length) {
-				log('Counted: ' + result).length;
-				callback(result.length);
-			} else {
-				log('No document(s) found with defined "find" criteria!');
-				callback(0);
-			}
-		});
+		waitForCollection(function() {
+			collection.find(query).toArray(function (err, result) {
+				if (err) {
+					log(err);
+				} else if (result.length) {
+					log('Counted: ' + result).length;
+					callback(result.length);
+				} else {
+					log('No document(s) found with defined "find" criteria!');
+					callback(0);
+				}
+			});
+		}.bind(this));
 	},
 
 	update: function(query, replacement) {
-		collection.update(query, {$set: replacement}, function (err, numUpdated) {
-			if (err) {
-				log(err);
-			} else if (numUpdated) {
-				log('Updated ' + numUpdated + ' objects in collection');
-			} else {
-				log('No document found with defined "find" criteria!');
-			}
+		waitForCollection(function() {
+			collection.update(query, {$set: replacement}, function (err, numUpdated) {
+				if (err) {
+					log(err);
+				} else if (numUpdated) {
+					log('Updated ' + numUpdated + ' objects in collection');
+				} else {
+					log('No document found with defined "find" criteria!');
+				}
 
-			callback();
-		});
+				callback();
+			});
+		}.bind(this));
 	},
 
 	remove: function(query, callback) {
-		collection.remove(query, function (err, numUpdated) {
-			if (err) {
-				log(err);
-			} else if (numUpdated) {
-				log('Removed ' + numUpdated + ' objects in collection');
-			} else {
-				log('No document found with defined "find" criteria!');
-			}
+		waitForCollection(function() {
+			collection.remove(query, function (err, numUpdated) {
+				if (err) {
+					log(err);
+				} else if (numUpdated) {
+					log('Removed ' + numUpdated + ' objects in collection');
+				} else {
+					log('No document found with defined "find" criteria!');
+				}
 
-			callback();
-		});
+				callback();
+			});
+		}.bind(this));
 	},
 
 	// These two functions are slightly hacky and just use Mongo as a big file
 	get: function(callback) {
-		this.find({}, function(data) {
-			data = data[0];
-			delete data['_id'];
-
-			callback(data);
-		});
+		waitForCollection(function() {
+			this.find({}, function(data) {
+				data = data[0];
+				delete data['_id'];
+				console.log(data);
+				callback(data);
+			});
+		}.bind(this));
 	},
 
 	set: function(data, callback) {
 		var self = this;
 
-		this.remove({}, function() {
-			self.insert(data, callback);
-		});
+		waitForCollection(function() {
+			this.remove({}, function() {
+				self.insert(data, callback);
+			});
+		}.bind(this));
 	}
 };
